b5f4ab8f429504b679a9830db031982e9e551419
Samuel W Linton <samuelwlinton@gmail.com>
2024-05-02 11:57:05 -0700

    Complete DistanceSensor

--- distance_sensor.py
+++ distance_sensor.py
@@ -2,10 +2,11 @@
 # DistanceSensor
 #
 from machine import Pin
-from time import sleep_us
+from time import sleep_us, ticks_us
 
 class DistanceSensor:
     """Class to get distance to features in front """
+    
     def __init__(self, trigger_pin_num, echo_pin_num)-> None:
         """_summary_
 
@@ -16,3 +17,29 @@ class DistanceSensor:
         
         self._trigger_pin = Pin(trigger_pin_num, Pin.OUT)
         self._echo_pin = Pin(echo_pin_num, Pin.IN)
+        
+    def get_distance_cm(self)-> float:
+        """Get distance in cm
+
+        Returns:
+            float: distance in cm
+        """
+        
+        self._trigger_pin.value(0)
+        sleep_us(2)
+        self._trigger_pin.value(1)
+        sleep_us(10)
+        self._trigger_pin.value(0)
+        
+        # Capture time of start of pulse
+        while self._echo_pin.value() == 0:
+            pass
+        t1 = ticks_us()
+        
+        # Capture time of end of pulse
+        while self._echo_pin.value() == 1:
+            pass
+        t2 = ticks_us()
+        
+        # Pulse duration indicates time of flight
+        return (t2 - t1) * 0.034 / 2
