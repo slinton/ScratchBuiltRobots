2fd08db6bf6f9f04a28cb424e0764d7a008bf186
Samuel W Linton <samuelwlinton@gmail.com>
2024-05-06 16:05:03 -0700

    Add comments

--- Bluetooth Dev/ble_server.py
+++ Bluetooth Dev/ble_server.py
@@ -1,6 +1,9 @@
 #
 # BLEServer
 #
+# TODO: should there be an additional non async function inside send_message for subclass
+# to override?
+
 import aioble
 import bluetooth
 from micropython import const
@@ -11,16 +14,30 @@ _GENERIC_CHAR_UUID = bluetooth.UUID(0x2A6E)
 _BLE_APPEARANCE_GENERIC_REMOTE_CONTROL = const(384)
 
 class BLEServer:
+    """Class to create a BLE server that advertises a service and characteristic.
+    This class can be instantiated and provided with a function that creates a message
+    to be sent by the server. The message is sent at a regular interval.
+    """
+    
     def __init__(self,
                  name:str,
                  send_message_func=None,
-                 send_interval_ms=1000,
-                 service_uuid=_GENERIC_SERVICE_UUID,
-                 char_uuid=_GENERIC_CHAR_UUID)->None:
+                 send_interval_ms:int=1000,
+                 service_uuid:bluetooth.UUID=_GENERIC_SERVICE_UUID,
+                 char_uuid:bluetooth.UUID=_GENERIC_CHAR_UUID)->None:
+        """Initialize the BLEServer object.
+
+        Args:
+            name (str): Name of the BLE server. This is used by the client to recognize the server.
+            send_message_func (_type_, optional): User-proviced function that provides a string to 
+                be sent by the server. Defaults to None.
+            send_interval_ms (int, optional): Interval between sends, in ms. Defaults to 1000.
+            service_uuid (UUID, optional): Service UUID. Defaults to _GENERIC_SERVICE_UUID.
+            char_uuid (UUID, optional): Characteristic UUID. Defaults to _GENERIC_CHAR_UUID.
+        """
         self.name = name
         self.send_message_func = send_message_func
         self.send_interval_ms = send_interval_ms
-        print(f'update_func = {self.update_func}')
         self.service_uuid = service_uuid
         self.char_uuid = char_uuid
         self.connected = False
@@ -29,6 +46,9 @@ class BLEServer:
         self.createService()
         
     def createService(self):
+        """Create the service and characteristic for the BLE server and registers
+        the service. This function is called during initialization.
+        """
         print('createService')
         service = aioble.Service(self.service_uuid)
         self.characteristic = aioble.Characteristic(
@@ -40,6 +60,12 @@ class BLEServer:
         aioble.register_services(service)
     
     async def advertise(self, interval_ms:int=250_000)->None:
+        """Advertise the BLE server. This function is called by the start function.
+        If the server is disconnected, it will resume advertising.
+
+        Args:
+            interval_ms (int, optional): Interval of advertising, ms. Defaults to 250_000.
+        """
         print('advertise')
         while True:
             print('start advertising')
@@ -51,12 +77,13 @@ class BLEServer:
             ) as self.connection:
                 print("connection from", self.connection.device)
                 self.connected = True
-
                 await self.connection.disconnected()
                 self.connected = False
                 print(f'disconnected')
     
     async def send_message(self)->None:
+        """Send a message at regular intervals. The message is created by the send_message_func
+        """
         while True:
             if self.connected:
                 print('send_message')
@@ -71,6 +98,8 @@ class BLEServer:
     
             
     async def start(self)->None:
+        """Start the BLE server. This function creates two tasks: advertise and send_message.
+        """
         print('start')
         tasks = [
             asyncio.create_task(self.advertise()),
@@ -81,6 +110,8 @@ class BLEServer:
     
     
 async def main()->None:
+    """Test Method
+    """
     bleServer = BLEServer('BLE Test')
     await bleServer.start()
     
