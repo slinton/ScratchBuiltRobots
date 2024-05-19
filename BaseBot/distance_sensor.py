4f8bd8d7bdae07fcbc78e10a5e738460b147865b
Samuel W Linton <samuelwlinton@gmail.com>
2024-05-19 16:05:04 -0700

    Better comments; easier argument names.

--- BaseBot/distance_sensor.py
+++ BaseBot/distance_sensor.py
@@ -7,16 +7,16 @@ from time import sleep_us, ticks_us
 class DistanceSensor:
     """Class to get distance to features in front """
     
-    def __init__(self, trigger_pin_num, echo_pin_num)-> None:
+    def __init__(self, trigger, echo)-> None:
         """_summary_
 
         Args:
-            trigger_pin_num (int): pin number for trigger
-            echo_pin_num (int): pin number for echo
+            trigger (int): pin number for trigger
+            echo (int): pin number for echo
         """
         
-        self._trigger_pin = Pin(trigger_pin_num, Pin.OUT)
-        self._echo_pin = Pin(echo_pin_num, Pin.IN)
+        self._trigger_pin = Pin(trigger, Pin.OUT)
+        self._echo_pin = Pin(echo, Pin.IN)
         
     def get_distance_cm(self)-> float:
         """Get distance in cm
@@ -24,22 +24,23 @@ class DistanceSensor:
         Returns:
             float: distance in cm
         """
-        
+
+        # Trigger the ultrasonic pulse
         self._trigger_pin.value(0)
         sleep_us(2)
         self._trigger_pin.value(1)
         sleep_us(10)
         self._trigger_pin.value(0)
         
-        # Capture time of start of pulse
+        # Start timing the signal from the echo pin
         while self._echo_pin.value() == 0:
             pass
         t1 = ticks_us()
         
-        # Capture time of end of pulse
+        # Finish timing the signal from the echo pin
         while self._echo_pin.value() == 1:
             pass
         t2 = ticks_us()
         
-        # Pulse duration indicates time of flight
+        # Echo pin signal duration indicates time of flight
         return (t2 - t1) * 0.034 / 2
