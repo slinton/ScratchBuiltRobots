3a0a719ca0d9f76e07357e915a963b5afb9cb76f
Samuel W Linton <samuelwlinton@gmail.com>
2024-05-19 19:28:11 -0700

    Add show_connected status light implementation

--- Joystick/joystick_controller.py
+++ Joystick/joystick_controller.py
@@ -7,12 +7,13 @@ from ble_server import BLEServer
 from time import sleep
 
 class JoystickController(BLEServer):
-    def __init__(self, i2c, left, right, debug)-> None:
+    def __init__(self, i2c, left: int, right: int, led: int=None, debug: bool = False)-> None:
         BLEServer.__init__(self, 
                            name="JoystickController",
                            send_interval_ms=1000)
         self.left_button = Pin(left, Pin.IN, Pin.PULL_UP)
         self.right_button = Pin(right, Pin.IN, Pin.PULL_UP)
+        self.led = None if led == None else Pin(led, Pin.OUT)
         self.adc = ADS1115(i2c, address=72, gain=1)
         self.rate = 4
         self.debug = debug
@@ -27,11 +28,19 @@ class JoystickController(BLEServer):
         button_code = left_value + 2 * right_value
         message = f'{ch0},{ch1},{ch2},{ch3},{button_code}'
         return message
+    
+    def show_connected(self, connected: bool)-> None:
+        if self.led == None:
+            return
+        if connected:
+            self.led.value(1)
+        else:
+            self.led.value(0)
 
 
 if __name__ == '__main__':
     i2c = I2C(1, scl=27, sda=26, freq=200_000)
-    joystickController = JoystickController(i2c=i2c, left=18, right=28, debug=True)
+    joystickController = JoystickController(i2c=i2c, left=18, right=28, led=1, debug=True)
     joystickController.start()
     
 
